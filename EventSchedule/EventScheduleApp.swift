1133b90fda5062240b47359454aece4490ca6706
Tom <twu@lcs.on.ca>
2023-02-07 11:33:55 -0500

    Initial Commit

---
+++ EventSchedule/EventScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventScheduleApp.swift
+//  EventSchedule
+//
+//  Created by Tom Wu on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct EventScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
